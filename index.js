89caa2890cd9bcf0505edd670d78f1b56a8748c0
qtanit87 <38641309+qtanit87@users.noreply.github.com>
2020-06-15 17:14:11 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const http = require('http');
 const port = process.env.PORT || 3000;
+const appinfo = require('./package.json');
+const envinfo = require('./environment.json');
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node!\n'
-  res.end(msg);
+  const msg = 'Hello World!'
+  res.write(`${msg} ${appinfo.name}-${appinfo.version} ${envinfo.envname}`);
+  res.end();
 });
+
 
 server.listen(port, () => {
   console.log(`Server running on http://localhost:${port}/`);
