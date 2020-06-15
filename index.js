d7ad0ae65cde65224b50df0ded1884b4a1f73e23
qtanit87 <38641309+qtanit87@users.noreply.github.com>
2020-06-15 09:05:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello World!'
+  const msg = 'Oddle Hello World!'
   res.write(msg);
   res.end();
 });
