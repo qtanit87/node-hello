0af5e5d664f39769e049532308bc9a1b46cfb10a
dxctrainer <dxc.trainer@gmail.com>
2020-06-15 14:17:04 +0700

    updating pipeline

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const http = require('http');
 const port = process.env.PORT || 3000;
 const appinfo = require('./package.json');
-const envinfo = require('./environment.json');
+//const envinfo = require('./environment.json');
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   const msg = 'Hello Node!\n'
-  res.write(`${msg} ${appinfo.name}-${appinfo.version} $(envinfo.envname)`);
+  res.write(`${msg} ${appinfo.name}-${appinfo.version}`);
   res.end();
 });
 
