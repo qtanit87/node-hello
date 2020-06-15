89e9e68b019b9b15f213b72a3310ade70988df1a
qtanit87 <38641309+qtanit87@users.noreply.github.com>
2020-06-15 09:51:33 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const http = require('http');
 const port = process.env.PORT || 3000;
-
+const appinfo = require('./package.json');
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   const msg = 'Oddle Hello!'
-  res.write(msg);
+  res.write(`${msg} ${appinfo.name}-${appinfo.version}`);
   res.end();
 });
 
